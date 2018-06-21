306401e4cbab90b0fe2e8771e966bd8352449b68
Alexander Roth <alexander.roth@bayer.com>
2018-06-21 14:37:09 +0200

    Clair should print all scan results; AquaSec sould print scanning summary

--- scan.sh
+++ scan.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 MICROSCANNER_TOKEN=${AQUA_SEC_TOKEN} bash aquasec/scan.sh ${CI_DOCKER_IMAGE_NAME} > result.txt
-if [ $? -eq 0 ]; then
-  echo OK
-else
-  echo FAIL
-fi
+
+sed -n '/vulnerability_summary/,/^}/ { x; /^$/! p; }' result.txt
